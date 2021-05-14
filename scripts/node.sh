97c701dbf4af665dc2510c1ac3c67604f09ca4d7
Evan Shortiss <evanshortiss@gmail.com>
2021-05-14 22:51:20 +0100

    chore: remove unused script

--- scripts/node.sh
+++
@@ -1,6 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker build -f Dockerfile.dev . -t shipwars-game-server
-docker run --rm \
--e CLUSTER_NAME="Local" -e HUSKY_SKIP_HOOKS=1 -p 8181:8181 \
--v "$(pwd)/src/:/usr/src/app/src/" --name=shipwars-game-server shipwars-game-server
